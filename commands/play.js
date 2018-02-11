ae65129b8c62476dcc24a6cbfb149b85ed36cf5d
Sahibdeep Nann <InitializeSahib@users.noreply.github.com>
2018-02-10 23:15:40 -0800

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,25 +1,15 @@
-
-
-const yt = require('ytdl-core');
-    exports.run = (client, message, args) => {
-
-    const voiceChannel = message.member.voiceChannel;
-    if (!voiceChannel){
-      return message.channel.sendMessage(":x: You are not in a voice channel!!");
-    }
-	message.channel.sendMessage(`:white_check_mark: **Connected to ` + voiceChannel + `!**`);
-    voiceChannel.join()
-    .then(connection => {
-	const args = message.content.split(" ").slice(1);
-      let stream = yt(args.join(" "), {audioonly: true});
-      yt.getInfo(args.join(" "), function(err, info) {
-      const title = info.title
-	  message.channel.send(`**Sure Thing! Now playing \ ${title}\.**`)
-      }) 
-      const dispatcher = connection.playStream(stream);
-      dispatcher.on('end', () => {
-       }).catch(e =>{
-         console.error(e);
-       });
+const yt = require('ytdl-core')
+exports.run = (client, message, args) => {
+  const voiceChannel = message.member.voiceChannel
+  if (!voiceChannel) return message.channel.sendMessage(':no_entry: **You are not in a voice channel.**')
+  voiceChannel.join().then(conn => {
+    message.channel.sendMessage(`:white_check_mark: **Connected to** ${voiceChannel}`)
+    const args = message.content.split(' ').slice(1)
+    const stream = yt(args.join(' '), {audioonly: true})
+    yt.getInfo(args.join(' '), (err, info) => {
+        message.channel.send(`:radio: **Now playing** ${info.title}`)
     })
-  }
+    const dispatcher = conn.playStream(stream)
+    dispatcher.on('end', () => {}).catch(console.error)
+  })
+}
