556656d446bdaaefb314ab0d7f71bbc2a32dbf4c
Sahibdeep Nann <InitializeSahib@users.noreply.github.com>
2018-02-10 23:07:28 -0800

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args, config) => {
-  message.channel.sendMessage("**:scream: " + message.author.username + " just slapped " + args.join(" ") + "!**");
+  message.channel.sendMessage(`:scream: ${message.author.username} **just slapped** ${args.join(' ')}!`)
 }
