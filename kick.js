23ba827fea29bce75b1ce2938a138b938aee37df
Sahibdeep Nann <InitializeSahib@users.noreply.github.com>
2018-02-10 23:03:01 -0800

    Update kick.js

--- kick.js
+++ kick.js
@@ -1,20 +1,11 @@
 exports.run = (client, message, [mention, ...reason]) => {
-  const modRole = message.guild.roles.find("name", "Mods");
-  if (!modRole)
-    return message.reply("The Mods role does not exist");
-
-  if (!message.member.roles.has(modRole.id))
-    return message.reply("You can't use this command.");
-
-  if (message.mentions.members.size === 0)
-    return message.reply("Please mention a user to kick");
-
-  if (!message.guild.me.hasPermission("KICK_MEMBERS"))
-    return message.reply("");
-
-  const kickMember = message.mentions.members.first();
-
-  kickMember.kick(reason.join(" ")).then(member => {
-    message.reply(`${member.user.username} was succesfully kicked.`);
-  });
-};
+  const modRole = message.guild.roles.find('name', 'Mods')
+  if (!modRole) return message.reply('The Mods role does not exist')
+  if (!message.member.roles.has(modRole.id)) return message.reply('You cannot use this command.')
+  if (message.mentions.members.size === 0) return message.reply('Please mention a user to kick')
+  if (!message.guild.me.hasPermission('KICK_MEMBERS')) return message.reply('I cannot kick members.')
+  const kickMember = message.mentions.members.first()
+  kickMember.kick(reason.join(' ')).then(member => {
+    message.reply(`${member.user.username} was succesfully kicked.`)
+  })
+}
