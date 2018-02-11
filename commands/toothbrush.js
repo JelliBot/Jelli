98a38b4fb35281a3ed0f0e2ecb88cb5f6efd3a5b
Sahibdeep Nann <InitializeSahib@users.noreply.github.com>
2018-02-10 23:04:42 -0800

    Prevented spam

--- commands/toothbrush.js
+++ commands/toothbrush.js
@@ -1,16 +1,5 @@
 exports.run = (client, message, args) => {
   message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
-  message.channel.send('https://www.youtube.com/watch?v=YyiZl9chxwM')
 }
 
 
