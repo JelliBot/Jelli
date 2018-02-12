1337c4769c55dc4b69ad496c36c8f6d14d5d1db8
Jelli <36350927+JelliBot@users.noreply.github.com>
2018-02-12 18:08:57 -0500

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,7 +1,5 @@
-const config = require('../config.json')
 exports.run = (client, message, args) => {
-  if(!message.author.id === config.blacklist) return
-  if (message.author.id !== config.ownerID) return
+  if (message.author.id !== process.env.ownerID) return
   if(!args || args.size < 1) return message.reply(':no_entry: **No command to reload!**')
   delete require.cache[require.resolve(`./${args[0]}.js`)]
   message.reply(`:white_check_mark: **OK**! Command ${args[0]} reloaded`)
