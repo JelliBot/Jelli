46f6c6ace3f88c0b9f9056e0ed70d6b4c94405bc
Jelli <36350927+JelliBot@users.noreply.github.com>
2018-02-12 18:06:08 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -1,6 +1,5 @@
-const config = require('../config.json')
 exports.run = (client, message, args) => {
-  if (message.author.id !== config.ownerID) return
+  if (message.author.id !== process.env.ownerID) return
   let command = message.content.replace(config.prefix + 'eval ', '')
   let output = eval(command)
   message.channel.send(`**INPUT**: \n \`\`\`` + args.join(' ') + ` \`\`\` \n **OUTPUT**: \n \`\`\`${output}\`\`\``)
