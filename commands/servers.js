9b54a3dcbe36072fa5691707e731790ec9be67cf
Jelli <36350927+JelliBot@users.noreply.github.com>
2018-02-12 18:10:03 -0500

    Update servers.js

--- commands/servers.js
+++ commands/servers.js
@@ -1,5 +1,3 @@
-const config = require('../config.json')
 exports.run = (client, message, args) => {
-  if (!message.author.id === config.blacklist) return
   message.channel.sendMessage(":raised_hands: We are on **" + client.guilds.size + "** servers!")
 }
