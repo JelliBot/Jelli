8efbf0ff3eb398039a93491ca3af68962e829090
Jelli <36350927+JelliBot@users.noreply.github.com>
2018-02-12 18:10:45 -0500

    Update setgame.js

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,6 +1,5 @@
-const config = require('../config.json')
 exports.run = (client, message, args, config) => {
-   if (message.author.id !== config.ownerID) return
+   if (message.author.id !== process.env.ownerID) return
    client.user.setGame(args.join(' '))
    message.channel.send(`:white_check_mark: **OK**! Game set to ${args.join(' ')}`)
 }
