1257f3df2d277480629575725411dcd08dbb46a7
Jelli <36350927+JelliBot@users.noreply.github.com>
2018-02-12 18:13:45 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,10 +19,10 @@ let servers = []
 
 client.on('message', message => {
   if (message.author.bot) return
-  if (message.content.indexOf(config.prefix) !== 0) return
+  if (message.content.indexOf(process.env.prefix) !== 0) return
   
   // This is the best way to define args. Trust me.
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
+  const args = message.content.slice(process.env.prefix.length).trim().split(/ +/g)
   const command = args.shift().toLowerCase()
   
   // The list of if/else is replaced with those simple 2 lines:
