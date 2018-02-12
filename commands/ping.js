6c684274fa6c44c6d9d35a2597e901d946034f9b
Jelli <36350927+JelliBot@users.noreply.github.com>
2018-02-12 18:08:00 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,4 @@
-const config = require('../config.json')
 exports.run = (client, message, args) => {
-  if (!message.author.id === config.blacklist) return
   message.channel.send(':clock130:').then(sent => {
     sent.edit(`:ping_pong: in **${sent.createdTimestamp - message.createdTimestamp}** ms`) 
   })
