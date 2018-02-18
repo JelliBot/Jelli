8c6663e79c051e28a6a1fa929bba7cb791412fff
Jelli <36350927+JelliBot@users.noreply.github.com>
2018-02-17 19:56:18 -0500

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -1,5 +1,5 @@
 const config = require('../config.json')
 exports.run = (client, message, args) => {
   if (!message.author.id === config.blacklist) return
-  message.channel.sendMessage("The command \"google\" is currently under maitinence. Check back later.");
+  message.channel.sendMessage("The command \"google\" is currently under maintenance. Check back later.");
 }
